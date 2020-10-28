1b4713aea492865c3da870cc7a865f49d92a001d
Rushi Chandalia <rushichandalia.ict17@gmail.com>
2020-10-28 18:15:10 +0530

    clearing things

--- App.js
+++ App.js
@@ -1,39 +1,12 @@
 import React from "react";
-import {
-  Text,
-  SafeAreaView,
-  View,
-  TouchableHighlight,
-  TouchableWithoutFeedback,
-} from "react-native";
-import styles from "./styles/home_style";
+import Home from "./components/home";
+
+import { View } from "react-native";
 
 export default function App() {
-  function login() {
-    console.log("LOGIN working");
-  }
-  function signup() {
-    console.log("signup working");
-  }
   return (
-    <SafeAreaView style={styles.home}>
-      <View style={styles.home_header}>
-        <View style={styles.header}>
-          <Text style={styles.header1}>GURUMUKH</Text>
-          <Text style={styles.header1}>STORE</Text>
-          <Text style={styles.header2}>STAY HOME, STAY SAFE</Text>
-        </View>
-      </View>
-      <View style={styles.login_container}>
-        <TouchableHighlight onPress={login} style={styles.login_button}>
-          <Text style={{ color: "white", fontWeight: "bold", fontSize: 15 }}>
-            LOGIN
-          </Text>
-        </TouchableHighlight>
-        <TouchableWithoutFeedback onPress={signup}>
-          <Text style={styles.signup_button}>Dont Have An Account?</Text>
-        </TouchableWithoutFeedback>
-      </View>
-    </SafeAreaView>
+    <View>
+      <Home />
+    </View>
   );
 }
--- components/home.js
+++ components/home.js
@@ -1,4 +1,42 @@
-import { StyleSheet } from "react-native";
+import React from "react";
+import {
+  Text,
+  SafeAreaView,
+  View,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+  StyleSheet,
+} from "react-native";
+
+export default function App() {
+  function login() {
+    console.log("LOGIN working");
+  }
+  function signup() {
+    console.log("signup working");
+  }
+  return (
+    <SafeAreaView style={styles.home}>
+      <View style={styles.home_header}>
+        <View style={styles.header}>
+          <Text style={styles.header1}>GURUMUKH</Text>
+          <Text style={styles.header1}>STORE</Text>
+          <Text style={styles.header2}>STAY HOME, STAY SAFE</Text>
+        </View>
+      </View>
+      <View style={styles.login_container}>
+        <TouchableHighlight onPress={login} style={styles.login_button}>
+          <Text style={{ color: "white", fontWeight: "bold", fontSize: 15 }}>
+            LOGIN
+          </Text>
+        </TouchableHighlight>
+        <TouchableWithoutFeedback onPress={signup}>
+          <Text style={styles.signup_button}>Dont Have An Account?</Text>
+        </TouchableWithoutFeedback>
+      </View>
+    </SafeAreaView>
+  );
+}
 
 const styles = StyleSheet.create({
   home: {
@@ -60,5 +98,3 @@ const styles = StyleSheet.create({
     letterSpacing: 1,
   },
 });
-
-export default styles;
